f193ca615c3fa8f03b01d7529bf3520f046ee0c1
Tom Vorel <tomvorel13@gmail.com>
2021-06-09 16:28:57 +0200

    Removing masonry grid for simplicity

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,29 +1,12 @@
 import * as React from "react"
-import Masonry from "react-masonry-css"
 import Header from "./header"
 import "../styles/main.scss"
 
 const Layout = ({ children }) => {
-  const breakpointColumnsObj = {
-    default: 4,
-    1400: 3,
-    770: 2,
-    500: 1,
-  }
-
   return (
     <>
       <Header />
-
-      <main className="container">
-        <Masonry
-          breakpointCols={breakpointColumnsObj}
-          className="my-masonry-grid"
-          columnClassName="my-masonry-grid_column"
-        >
-          {children}
-        </Masonry>
-      </main>
+      <main className="container">{children}</main>
     </>
   )
 }
